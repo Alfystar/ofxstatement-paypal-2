64355fc056fb098acdebfb63bc6db56006ac82ca
EmanueleAlfano <emanuele.alfano@enel.com>
2025-03-03 12:50:56 +0100

    Update version to 3.0.0-rc1 and adjust development status in setup.py; fix formatting in README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_namespace_packages
 from distutils.core import setup
 
 setup(name='ofxstatement-paypal-2',
-      version='3.0.0',
+      version='3.0.0-rc1',
       author='Alfystar',
       author_email='alfystar1701@gmail.com',
       url='https://github.com/Alfystar/ofxstatement-paypal.git',
@@ -15,7 +15,7 @@ setup(name='ofxstatement-paypal-2',
       license='Apache License 2.0',
       keywords=['ofx', 'ofxstatement', 'paypal'],
       classifiers=[
-          'Development Status :: 3 - Alpha',
+          'Development Status :: 5 - Production/Stable',
           'Programming Language :: Python :: 3',
           'Natural Language :: English',
           'Topic :: Office/Business :: Financial :: Accounting',
